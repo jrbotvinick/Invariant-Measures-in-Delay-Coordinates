cf5a96f0bda866c63f08ab602ecc7cca494be37f
jrbotvinick <100333155+jrbotvinick@users.noreply.github.com>
2024-12-16 13:56:21 -0500

    Update NN_measure_loss.py

--- NN_measure_loss.py
+++ NN_measure_loss.py
@@ -21,10 +21,10 @@ num_steps = int(1e6) #length of trajectory simulation
 y = np.zeros((num_steps + 1, 3))  
 y[0] = np.array([-5.065457, -7.56735 , 19.060379])  # Set initial values
 tau = 100 #time-delay
-method = 'delay_IM' #select either 'delay_IM' or 'IM' to choose between a delay-coordinate invariant measure loss or a state-coordinate invariant measure loss
+method = 'IM' #select either 'delay_IM' or 'IM' to choose between a delay-coordinate invariant measure loss or a state-coordinate invariant measure loss
 num_samples = 2000 #the number of trajectory samples we want to learn from. The runtime is very sensitive to this number since we do not use minibatches. For > 2000, one should partition into mini-batches. 
 Nsteps = 30000 #how many training iterations to take 
-plot_every = 500
+plot_every = 500 #how often to plot training result
 
 ###############################################
 
@@ -56,7 +56,7 @@ batch_ixs = list(range(0,len(y)))
 ixs = random.sample(batch_ixs,num_samples)
 
 ############################## Build network 
-torch.manual_seed(12932) #random seed so that initialization is controlled for comparison
+torch.manual_seed(1293452) #random seed so that initialization is controlled for comparison
 
 net = nn.Sequential(
     nn.Linear(3, 100),
@@ -69,43 +69,42 @@ net = nn.Sequential(
 
 ############################## Define Loss
 loss = SamplesLoss(loss="energy")
-optimizer = optim.Adam(net.parameters(), lr=1e-3)  
-##############################  training loop
-
+optimizer = optim.Adam(net.parameters(), lr=1e-3)
+  
+##############################  Training loop
 net.train()
 losses = []
 for step in range(Nsteps):
-    y_batch = torch.tensor(y[ixs],dtype = torch.float,requires_grad = True)    
-    Ty_batch = torch.tensor(Ty_true[ixs],dtype = torch.float,requires_grad = True) 
-    y_delay_batch = torch.tensor(y_delay[ixs],dtype = torch.float,requires_grad = True)
+    y_batch = torch.tensor(y[ixs],dtype = torch.float,requires_grad = True)     #\mu_*
+    Ty_batch = torch.tensor(Ty_true[ixs],dtype = torch.float,requires_grad = True)  #T_*\#\mu_*
+    y_delay_batch = torch.tensor(y_delay[ixs],dtype = torch.float,requires_grad = True) #\Psi_*\#\mu_*
     optimizer.zero_grad()      
-    Ty = net(y_batch)
+    Ty = net(y_batch) #T_{\theta}\#\mu_*
     TTy = net(Ty)
     TTTy = net(TTy)
     TTTTy = net(TTTy)
     TTTTTy = net(TTTTy)
-
-    DIM = torch.cat((TTTTTy[:,0].unsqueeze(0),TTTTy[:,0].unsqueeze(0),TTTy[:,0].unsqueeze(0),TTy[:,0].unsqueeze(0),Ty[:,0].unsqueeze(0),y_batch[:,0].unsqueeze(0)),dim = 0).T
+    DIM = torch.cat((TTTTTy[:,0].unsqueeze(0),TTTTy[:,0].unsqueeze(0),TTTy[:,0].unsqueeze(0),TTy[:,0].unsqueeze(0),Ty[:,0].unsqueeze(0),y_batch[:,0].unsqueeze(0)),dim = 0).T #\Psi_{\theta}\#\mu_*
     #delay invariant measure
     if method =='delay_IM':
         L = loss(Ty,Ty_batch) + loss(DIM,y_delay_batch)
     if method == 'IM':
         L = loss(Ty,Ty_batch)
     losses.append(L.detach().numpy())
-    # L = loss(Ty,y[1:])
-    # L = torch.mean((Ty-y[1:])**2)
     
     print('iteration ', step, 'loss ', L)
     L.backward()
     optimizer.step()
     if step%plot_every == 0:
         # state coordinate push forward
+        plt.title('State-Coordinate Pushforward Samples',fontsize = 15)
         plt.scatter(Ty.detach().numpy()[:,0],Ty.detach().numpy()[:,1],s =1)
         plt.scatter(Ty_batch.detach().numpy()[:,0],Ty_batch.detach().numpy()[:,1], s =1)
         plt.xlim(-1,1)
         plt.ylim(-1,1)
         plt.show()
         #delay coordinate push forward
+        plt.title('Delay-Coordinate Pushforward Samples',fontsize = 15)
         plt.scatter(DIM.detach().numpy()[:,0],DIM.detach().numpy()[:,1],s =1)
         plt.scatter(y_delay_batch.detach().numpy()[:,0],y_delay_batch.detach().numpy()[:,1], s =1)
         plt.xlim(-1,1)
@@ -120,7 +119,9 @@ for step in range(Nsteps):
         xs = transformer.inverse_transform(np.array(xs))
         plt.scatter(xs[:,0],xs[:,2],s = .1)
         plt.show()
-        
+
+
+#Simulate a long trajectory using the learned model        
 net.eval()
 x = torch.tensor(y[0],dtype = torch.float)
 xs = []
