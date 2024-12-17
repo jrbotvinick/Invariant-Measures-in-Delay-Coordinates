2d3e410b9697db70de32ec0de0c5c7edb42df5de
jrbotvinick <100333155+jrbotvinick@users.noreply.github.com>
2024-12-17 16:36:38 -0500

    Update NN_measure_loss.py

--- NN_measure_loss.py
+++ NN_measure_loss.py
@@ -22,10 +22,9 @@ y = np.zeros((num_steps + 1, 3))
 y[0] = np.array([-5.065457, -7.56735 , 19.060379])  # Set initial values
 tau = 100 #time-delay
 method = 'IM' #select either 'delay_IM' or 'IM' to choose between a delay-coordinate invariant measure loss or a state-coordinate invariant measure loss
-num_samples = 2000 #the number of trajectory samples we want to learn from. The runtime is very sensitive to this number since we do not use minibatches. For > 2000, one should partition into mini-batches. 
+num_samples = 1000 #the number of trajectory samples we want to learn from. The runtime is very sensitive to this number since we do not use minibatches. For > 2000, one should partition into mini-batches. 
 Nsteps = 30000 #how many training iterations to take 
 plot_every = 500 #how often to plot training result
-
 ###############################################
 
 #simulate trajectory
@@ -75,49 +74,67 @@ optimizer = optim.Adam(net.parameters(), lr=1e-3)
 net.train()
 losses = []
 for step in range(Nsteps):
-    y_batch = torch.tensor(y[ixs],dtype = torch.float,requires_grad = True)     #\mu_*
-    Ty_batch = torch.tensor(Ty_true[ixs],dtype = torch.float,requires_grad = True)  #T_*\#\mu_*
-    y_delay_batch = torch.tensor(y_delay[ixs],dtype = torch.float,requires_grad = True) #\Psi_*\#\mu_*
-    optimizer.zero_grad()      
-    Ty = net(y_batch) #T_{\theta}\#\mu_*
+    #\mu_*
+    y_batch = torch.tensor(y[ixs],dtype = torch.float,requires_grad = True)     
+    
+    #T_*\#\mu_*
+    Ty_batch = torch.tensor(Ty_true[ixs],dtype = torch.float,requires_grad = True)  
+    
+    #\Psi_*\#\mu_*
+    y_delay_batch = torch.tensor(y_delay[ixs],dtype = torch.float,requires_grad = True) 
+    optimizer.zero_grad()
+    
+    #T_{\theta}\#\mu_*
+    Ty = net(y_batch) 
     TTy = net(Ty)
     TTTy = net(TTy)
     TTTTy = net(TTTy)
     TTTTTy = net(TTTTy)
-    DIM = torch.cat((TTTTTy[:,0].unsqueeze(0),TTTTy[:,0].unsqueeze(0),TTTy[:,0].unsqueeze(0),TTy[:,0].unsqueeze(0),Ty[:,0].unsqueeze(0),y_batch[:,0].unsqueeze(0)),dim = 0).T #\Psi_{\theta}\#\mu_*
-    #delay invariant measure
+    
+    #\Psi_{\theta}\#\mu_*
+    DIM = torch.cat((TTTTTy[:,0].unsqueeze(0),TTTTy[:,0].unsqueeze(0),TTTy[:,0].unsqueeze(0),TTy[:,0].unsqueeze(0),Ty[:,0].unsqueeze(0),y_batch[:,0].unsqueeze(0)),dim = 0).T 
+    
+    
+    #dChoose between state-coordinate or delay-coordinate invariant measure loss
     if method =='delay_IM':
         L = loss(Ty,Ty_batch) + loss(DIM,y_delay_batch)
     if method == 'IM':
         L = loss(Ty,Ty_batch)
-    losses.append(L.detach().numpy())
-    
-    print('iteration ', step, 'loss ', L)
+        
+        
+    losses.append(L.detach().numpy())  
+    print('iteration : ', step, '| loss :', L.detach().numpy())
     L.backward()
     optimizer.step()
+    
+    #plot current progress
     if step%plot_every == 0:
         # state coordinate push forward
         plt.title('State-Coordinate Pushforward Samples',fontsize = 15)
-        plt.scatter(Ty.detach().numpy()[:,0],Ty.detach().numpy()[:,1],s =1)
-        plt.scatter(Ty_batch.detach().numpy()[:,0],Ty_batch.detach().numpy()[:,1], s =1)
+        plt.scatter(Ty_batch.detach().numpy()[:,0],Ty_batch.detach().numpy()[:,1], s =1,label = 'Data')
+        plt.scatter(Ty.detach().numpy()[:,0],Ty.detach().numpy()[:,1],s =1, label = 'Model')
+        plt.legend(loc = 'upper right')
         plt.xlim(-1,1)
         plt.ylim(-1,1)
         plt.show()
         #delay coordinate push forward
         plt.title('Delay-Coordinate Pushforward Samples',fontsize = 15)
-        plt.scatter(DIM.detach().numpy()[:,0],DIM.detach().numpy()[:,1],s =1)
-        plt.scatter(y_delay_batch.detach().numpy()[:,0],y_delay_batch.detach().numpy()[:,1], s =1)
+        plt.scatter(y_delay_batch.detach().numpy()[:,0],y_delay_batch.detach().numpy()[:,1], s =1,label = 'Data')
+        plt.scatter(DIM.detach().numpy()[:,0],DIM.detach().numpy()[:,1],s =1,label = 'Model')
+        plt.legend(loc = 'upper right')
+
         plt.xlim(-1,1)
         plt.ylim(-1,1)
         plt.show()
-        #traj simulation
+        #plot a long simulated trajectory
         x = y_batch[0]
         xs = []
         for i in range(int(1e4)):
             xs.append(x.detach().numpy())
             x  = net(x)
         xs = transformer.inverse_transform(np.array(xs))
-        plt.scatter(xs[:,0],xs[:,2],s = .1)
+        plt.title('Model Simulated Trajectory',fontsize = 15)
+        plt.plot(xs[:,0],xs[:,2])
         plt.show()
 
 
@@ -136,6 +153,8 @@ plt.show()
         
 plt.plot(xs[:,0][:5000][5:],xs[:,2][:5000][5:],linewidth = .5,linestyle = '--',marker = 'o',markersize = 3) 
 
+
+#save the data for plotting
 if method == 'delay_IM': 
     with open("DIM_recon.p", "wb") as f:
         pickle.dump([y,y[ixs],xs,losses], f)
@@ -143,5 +162,3 @@ if method == 'IM':
     with open("IM_recon.p", "wb") as f:
         pickle.dump([y,y[ixs],xs,losses], f)
 
-plt.plot(losses)
-plt.yscale('log')
